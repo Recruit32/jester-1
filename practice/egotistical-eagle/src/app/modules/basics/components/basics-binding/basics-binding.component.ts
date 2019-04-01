bfda926c379a74531f59743d61f0e26e7a76a767
Recruit32 <recruit.a32@gmail.com>
2019-04-01 15:25:45 -0700

    basics-binding-component

--- practice/egotistical-eagle/src/app/modules/basics/components/basics-binding/basics-binding.component.ts
+++ practice/egotistical-eagle/src/app/modules/basics/components/basics-binding/basics-binding.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'basics-binding',
   templateUrl: './basics-binding.component.html',
   styleUrls: ['./basics-binding.component.scss']
 })
-export class BasicsBindingComponent {
-  
+export class BasicsBindingComponent implements OnInit {
+  petName: string = 'Roar';
+
+  ngOnInit() {
+
+  }
+
+  onUpdatePetName(event: Event) {
+    this.petName = (<HTMLInputElement>event.target).value;
+  }
 }
